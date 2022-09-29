4a8d84a0f8323ba78fb072d775332d6ba1b05b9e
İbrahim Uyanık <ibrahimhakkiuyanik@outlook.com>
2022-09-29 14:43:20 +0300

    .

---
+++ deneme2/Controllers/HelloWorldController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace deneme2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
